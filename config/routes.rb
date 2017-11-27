ec9bf31654a10d2f0cc9e0afc1a75d7cccc39852
KahTim <kahtim.tang@gmail.com>
2017-11-27 17:05:16 +0800

    done w omniauthfb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, controller: "clearance/users", only: [:create] do
+  resources :users, controller: "clearance/users", only: [:create, :edit, :update] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
 end
