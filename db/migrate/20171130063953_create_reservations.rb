9eab96a72ca61e84b06276f872acc3bfcb50b4ff
KahTim <kahtim.tang@gmail.com>
2017-11-30 15:41:11 +0800

    migrated reservation

--- db/migrate/20171130063953_create_reservations.rb
+++ db/migrate/20171130063953_create_reservations.rb
@@ -2,8 +2,11 @@ class CreateReservations < ActiveRecord::Migration[5.1]
   def change
     create_table :reservations do |t|
 
-    	x.references :user, foreign_key: true
-    	x.references :listing, foreign_key: true
+    	t.date :start_date
+    	t.date :end_date
+    	t.integer :duration 
+    	t.references :user, foreign_key: true
+    	t.references :listing, foreign_key: true
 
       t.timestamps
     end
