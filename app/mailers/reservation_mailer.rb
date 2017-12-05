49a15bb14afa862580b48f9d7ccca20d518be199
KahTim <kahtim.tang@gmail.com>
2017-12-05 19:20:32 +0800

    before gmail

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,5 +1,13 @@
 class ReservationMailer < ApplicationMailer
+
+	default from: 'reservations@example.com'
+
 	def booking_email(customer, host, reservation_id)
-  	...
+  	@customer = customer
+  	@host = host
+  	@reservation_id = reservation_id
+  	# byebug
+   #  @url  = 'http://example.com/login'
+    mail(to: @host.email, subject: 'Welcome to My Awesome Site')
   end
 end
