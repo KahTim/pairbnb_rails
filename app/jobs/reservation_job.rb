e90148bfd1a770cf4f3bee041361bb85005ab32c
KahTim <kahtim.tang@gmail.com>
2017-12-06 15:21:08 +0800

    done w revising params for mailer job

--- app/jobs/reservation_job.rb
+++ app/jobs/reservation_job.rb
@@ -2,9 +2,10 @@ class ReservationJob < ApplicationJob
   queue_as :default
 
   def perform(reservation) #work on this
-  	x = Reservation.last
-  	y = Listing.find(x.listing_id)
-  	ReservationMailer.booking_email(User.find(x.user_id), User.find(y.user_id), x.id).deliver_now
+  	@reservation = reservation
+  	# x = Reservation.last
+  	y = Listing.find(@reservation.listing_id)
+  	ReservationMailer.booking_email(User.find(@reservation.user_id), User.find(y.user_id), @reservation.id).deliver_now
     # Do something later
   end
 end
