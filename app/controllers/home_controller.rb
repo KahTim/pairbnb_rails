ab692390b2122680a2575b2ce5866445b3bba9e4
KahTim <kahtim.tang@gmail.com>
2017-12-06 15:59:41 +0800

    fixed index page no method error nil class for current_user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-  	@user = User.find(current_user.id)
+  	if current_user
+  		@user = User.find(current_user.id)
+  	end
   end
   
 end
